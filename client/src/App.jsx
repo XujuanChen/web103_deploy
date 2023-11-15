013ef8863225ccea6768d9945cc5e24490195ad3
XujuanChen <xchen022@citymail.cuny.edu>
2023-11-14 21:50:14 -0500

    server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,8 +15,7 @@ import Avatar from './components/Avatar'
 import AddUserToTrip from './pages/AddUserToTrip'
 
 const App = () => {
-  const API_URL = 'http://localhost:3001'
-  // const API_URL = process.env.NODE_ENV === 'production' ? 'https://fredngo-cp-w103-lab9-server.up.railway.app' : 'http://localhost:3001'
+  const API_URL = process.env.NODE_ENV === 'production' ? 'web103deploy-server.up.railway.app' : 'http://localhost:3001'
 
   const [trips, setTrips] = useState([]);
   const [user, setUser] = useState([])
